90b08e809a8da49625a43da41e12f6db8ff83ff6
Alejandro Díaz <67884952+alejandrojosue@users.noreply.github.com>
2023-09-01 09:13:11 -0600

    Update create.js

--- js/products/create.js
+++ js/products/create.js
@@ -1,7 +1,20 @@
 import ProductsController from '../../controllers/productsController.js'
 const productsController = new ProductsController();
 const create = async () => {
-    const name = document.getElementById("productName").value
+    
+}
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach((form) => {
+        form.addEventListener('submit', async(event) => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+            if (form.checkValidity()) {
+                const name = document.getElementById("productName").value
     const stock = parseInt(document.getElementById("stock").value)
     const unitPrice = parseFloat(document.getElementById("unitPrice").value)
     const description = document.getElementById("description").value
@@ -17,19 +30,6 @@ const create = async () => {
             console.error(err);
         }
     }
-}
-(() => {
-    'use strict'
-    const forms = document.querySelectorAll('.needs-validation')
-    Array.from(forms).forEach((form) => {
-        form.addEventListener('submit', (event) => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-            form.classList.add('was-validated')
-            if (form.checkValidity()) {
-                create()
             }
         }, false)
     })
