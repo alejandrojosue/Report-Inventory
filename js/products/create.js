d14b221240d9d04f7ac17b0fde51748d728a97ec
Alejandro Díaz <67884952+alejandrojosue@users.noreply.github.com>
2023-09-01 13:05:29 -0600

    Update create.js

--- js/products/create.js
+++ js/products/create.js
@@ -11,8 +11,9 @@ const description = document.getElementById("description");
     const forms = document.querySelectorAll('.needs-validation')
     Array.from(forms).forEach((form) => {
         form.addEventListener('submit', async(event) => {
+            event.preventDefault()
             if (!form.checkValidity()) {
-                event.preventDefault()
+                
                 event.stopPropagation()
             }
             form.classList.add('was-validated')
